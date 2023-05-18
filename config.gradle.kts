cc092b8b3fae25b65d11be3934efe87369a8d24a
MoahmedKhairyAbdella <mohamed.k.abdellatif@paymob.com>
2023-05-18 19:43:21 +0300

    set config file

---
+++ config.gradle.kts
@@ -0,0 +1,17 @@
+extra.apply {
+  set("applicationId", "com.example.football")
+  set("compileSdk", 33)
+  set("minSdk", 24)
+  set("versionCode", 1)
+  set("versionName", "1.0")
+  set("testRunner", "androidx.test.runner.AndroidJUnitRunner")
+
+
+  // Build Types
+  set("debugBuildType", "debug")
+  set("releaseBuildType", "release")
+
+  // Compose Version
+  set("composeVersion", "1.4.4")
+
+}
