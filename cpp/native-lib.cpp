41bed6966db2074e00f3f607bfce2108c52ff77a
MoahmedKhairyAbdella <mohamed.k.abdellatif@paymob.com>
2023-05-20 04:21:02 +0300

    add api key

---
+++ cpp/native-lib.cpp
@@ -0,0 +1,25 @@
+#include <jni.h>
+#include <string>
+
+
+extern "C" jstring Java_com_example_home_data_remote_service_galleryPhotos_FootballServiceImpl_getBaseUrlFromNative(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string baseURL = "https://api.football-data.org/v2/";
+    return env->NewStringUTF(baseURL.c_str());
+}
+
+
+extern "C" jstring Java_com_example_network_Headers_getApiKeyName(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string keyName = "X-Auth-Token";
+    return env->NewStringUTF(keyName.c_str());
+}
+
+extern "C" jstring Java_com_example_network_Headers_getApiKeyValue(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string keyValue = "965c87cec54f484e9b2de4c841e6ca3a";
+    return env->NewStringUTF(keyValue.c_str());
+}
